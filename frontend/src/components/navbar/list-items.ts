28279e18e9100003acc67cce5ce2da66a4ed6148
sixLUKEY <marcoslukerojo@gmail.com>
2024-09-15 19:30:40 +0200

    feat(#6): Added basic navbar

---
+++ frontend/src/components/navbar/list-items.ts
@@ -0,0 +1,11 @@
+interface ListItems {
+  name: string;
+  path: string;
+}
+
+export const listItems: ListItems[] = [
+  { name: "Home", path: "home" },
+  { name: "About", path: "about" },
+  { name: "Services", path: "services" },
+  { name: "Contact", path: "contact" },
+];
