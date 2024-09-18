959c3bb35e9655afe475a385ad4407b88ad03339
sixLUKEY <marcoslukerojo@gmail.com>
2024-09-18 12:58:24 +0200

    feat(#6): Added some fonts, colors, components

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,25 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			colors: {
-				surface: "",
-				onSurface: "",
-				surfaceDark: "",
-				onSurfaceDark: "",
-				container: "",
-				containerDark: "",
-				onContainer: "",
-				onContainerDark: "",
-				primary: "",
-				primaryDark: "",
-				secondary: "",
-				secondaryDark: "",
-				tertiary: "",
-				tertiaryDark: "",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ["Bebas Neue", "sans-serif"],
+      },
+      colors: {
+        surface: "",
+        onSurface: "",
+        surfaceDark: "",
+        onSurfaceDark: "",
+        container: "",
+        containerDark: "",
+        onContainer: "",
+        onContainerDark: "",
+        primary: "",
+        primaryDark: "#F1BB27",
+        secondary: "",
+        secondaryDark: "",
+        tertiary: "",
+        tertiaryDark: "",
+        alternate: "",
+        alternateDark: "#FFDF3A",
+      },
+    },
+  },
+  plugins: [],
 };
